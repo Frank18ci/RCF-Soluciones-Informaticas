6f742f58bde00aced654ea4a195ee52cbce03486
Frank <fcarpio113@gmail.com>
2025-09-30 20:42:19 -0500

    Agregando colores personalizados front end

---
+++ front end/rcf-soluciones-informaticas-angular/src/app/tienda/pages/contactanos-page/contactanos-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contactanos-page',
+  imports: [],
+  templateUrl: './contactanos-page.html',
+  styleUrl: './contactanos-page.css'
+})
+export class ContactanosPage {
+
+}
--- front end/rcf-soluciones-informaticas-angular/src/app/tienda/tienda-routing-module.ts
+++ front end/rcf-soluciones-informaticas-angular/src/app/tienda/tienda-routing-module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioPage } from './pages/inicio-page/inicio-page';
 import { LayoutPage } from './pages/layout-page/layout-page';
+import { ContactanosPage } from './pages/contactanos-page/contactanos-page';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: 'inicio',
         component: InicioPage
+      },
+      {
+        path: 'contactanos',
+        component: ContactanosPage
       },
       {
         path: '**',
