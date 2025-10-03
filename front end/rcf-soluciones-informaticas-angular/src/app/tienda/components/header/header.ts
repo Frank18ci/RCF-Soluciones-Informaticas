e39189b95be53733694f5eb5a794c5ddbf2f1b2f
Frank <fcarpio113@gmail.com>
2025-10-02 19:00:54 -0500

    Terminando v1 de pagina tienda/inicio

--- front end/rcf-soluciones-informaticas-angular/src/app/tienda/components/header/header.ts
+++ front end/rcf-soluciones-informaticas-angular/src/app/tienda/components/header/header.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Topbar } from '../topbar/topbar';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MaterialModule } from '../../../shared/modules/material/material-module';
+import { BehaviorSubject, fromEvent, map, Observable, startWith, Subscription, throttleTime } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [Topbar, FormsModule, RouterLink, MatIconModule, MaterialModule],
+  imports: [Topbar, FormsModule, RouterLink, MatIconModule, MaterialModule, AsyncPipe],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
-export class Header {
+export class Header implements OnInit, OnDestroy {
+   showTopbar: Observable<boolean> = new BehaviorSubject(true);
+
+
+  ngOnInit() {
+    this.showTopbar = fromEvent(window, 'scroll').pipe(
+      throttleTime(50),
+      map(() => window.scrollY > 100),
+      startWith(window.scrollY > 100)
+    );
+    
+  }
+  ngOnDestroy() {
+    
+  }
   isMenuOpen = false;
   cartCount = 0;
   q = '';
