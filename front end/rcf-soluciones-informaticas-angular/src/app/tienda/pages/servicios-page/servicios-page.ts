da7abe0bcc6002c7d7d02a1ffc6b6291c2615d19
Frank <fcarpio113@gmail.com>
2025-10-02 21:34:24 -0500

    Version inicial de catalogo de productos

---
+++ front end/rcf-soluciones-informaticas-angular/src/app/tienda/pages/servicios-page/servicios-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servicios-page',
+  imports: [],
+  templateUrl: './servicios-page.html',
+  styleUrl: './servicios-page.css'
+})
+export class ServiciosPage {
+
+}
