3358a7c3ea96221033654247ccfc437b53afcfcc
RRRA00 <I202319209@cibertec.edu.pe>
2025-10-08 18:26:18 -0500

    Apartado contactanos

--- front end/rcf-soluciones-informaticas-angular/src/app/tienda/pages/contactanos-page/contactanos-page.ts
+++ front end/rcf-soluciones-informaticas-angular/src/app/tienda/pages/contactanos-page/contactanos-page.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contactanos-page',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, MatIconModule],
   templateUrl: './contactanos-page.html',
   styleUrl: './contactanos-page.css'
 })
 export class ContactanosPage {
+  formData = {
+    nombre: '',
+    email: '',
+    telefono: '',
+    asunto: '',
+    mensaje: ''
+  };
 
+  onSubmit() {
+    if (!this.formData.nombre || !this.formData.email || !this.formData.mensaje) {
+      alert('Por favor, completa todos los campos obligatorios.');
+      return;
+    }
+
+    console.log('Formulario enviado:', this.formData);
+    alert('¡Gracias por contactarnos! Te responderemos pronto.');
+
+    
+    this.formData = {
+      nombre: '',
+      email: '',
+      telefono: '',
+      asunto: '',
+      mensaje: ''
+    };
+  }
 }
